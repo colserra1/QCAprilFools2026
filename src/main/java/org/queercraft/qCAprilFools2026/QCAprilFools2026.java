7ba4e25e8872d003b3fb63a4646a73a5095b1711
colserra <info@colserra.dev>
2026-03-31 21:57:05 +0200

    Initial commit

---
+++ src/main/java/org/queercraft/qCAprilFools2026/QCAprilFools2026.java
@@ -0,0 +1,17 @@
+package org.queercraft.qCAprilFools2026;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QCAprilFools2026 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
